3a13592638a462ad081810e660af5ae702acdcbe
Nookom <steven.a.morgan2@gmail.com>
2025-01-25 13:46:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,17 +4,43 @@ const audioElement = document.getElementById('daily-audio');
 const body = document.body;
 
 const poems = [
-    'Poem 1',
-    'Poem 2',
-    'Poem 3',
+    'Charlie is my golden, longhaired sin.
+His eyes tell all his dark brown chocolate thoughts.
+All he wants is loving without end,
+Ravenous for touching of all sorts.
+Left on his own, he cannot comprehend
+In lonely misery how I defend
+Even just one breath away from him.
+Copyright by Nicholas Gordon',
+    'Grandmothers are mothers who are grand,
+Restoring the sense that our most precious things
+Are those that do not change much over time.
+No love of childhood is more sublime,
+Demanding little, giving on demand,
+More inclined than most to grant the wings
+On which we fly off to enchanted lands.
+Though grandmothers must serve as second mothers,
+Helping out with young and restless hearts,
+Each has all the patience wisdom brings,
+Remembering our passions more than others,
+Soothing us with old and well-honed arts.
+Copyright by Nicholas Gordon',
+    'There is no greater passion than for beauty --
+Ecstasy distilled into a song --
+Nor calling more exquisite than the duty
+To make our own the truths for which we long.
+Here's to you, then! And for what you've done
+To be the muse who mirrors well our hearts,
+Restoring the lone many to the one
+Common love that underlies all arts.
+O love of being, bearer of our pain!
+Well might we praise the gardeners who bring
+Our passions into bloom, that we again
+Might hear the sunlit bird within us sing.
+Long may you ply what practices you've learned,
+Profiting all by artistry you've earned.
+Copyright by Nicholas Gordon',
     // Add more poems here...
-];
-
-const songs = [
-    { title: 'Song 1', artist: 'Artist 1', audio: 'song1.mp3' },
-    { title: 'Song 2', artist: 'Artist 2', audio: 'song2.mp3' },
-    { title: 'Song 3', artist: 'Artist 3', audio: 'song3.mp3' },
-    // Add more songs here...
 ];
 
 const backgrounds = [
